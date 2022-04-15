32c9867d05017b841898fa34b2b8388e03566841
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2022-04-15 19:56:47 +0100

    Pull latest changes from https://github.com/github/choosealicense.com

--- pychoosealicense/_licenses/__init__.py
+++ pychoosealicense/_licenses/__init__.py
@@ -3,12 +3,15 @@ License metadata used by ``pychoosealicense``.
 """
 
 CHOOSEALICENSE_COMMIT_INFO = """
-commit 20d853259a9412aa7a0d6f6d9bc0f098f38266d9
+commit 48c1e44d1dff919ca8764d8567b9fce62ec3e7e6
+Merge: 639dac9 242b063
 Author: Mike Linksvayer <mlinksva@github.com>
-Date:   Wed Jan 5 17:50:10 2022 -0800
+Date:   Thu Apr 14 16:53:19 2022 -0700
 
-    rubocop suggestions
+    Merge pull request #981 from liyishuai/mulan
+
+    mulan: fix wrapping and trim space
 """
 
-CHOOSEALICENSE_COMMIT_HASH = "20d853259a9412aa7a0d6f6d9bc0f098f38266d9"
-CHOOSEALICENSE_COMMIT_SHORTHASH = "20d8532"
+CHOOSEALICENSE_COMMIT_HASH = "48c1e44d1dff919ca8764d8567b9fce62ec3e7e6"
+CHOOSEALICENSE_COMMIT_SHORTHASH = "48c1e44"
