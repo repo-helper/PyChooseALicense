bee79c0af6949a92a18766171d2ce812c801eed3
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2023-03-13 11:49:40 +0000

    Update licenses from upstream

--- pychoosealicense/_licenses/__init__.py
+++ pychoosealicense/_licenses/__init__.py
@@ -3,15 +3,15 @@ License metadata used by ``pychoosealicense``.
 """
 
 CHOOSEALICENSE_COMMIT_INFO = """
-commit 62dfebe236ac92b5627f4f7cf96650801c5bdb89
-Merge: 695f9ca cf8f0d7
+commit 0ccd6c5d0ce1c0f68e088ec0c936322e43b2fbfb
+Merge: 2b3259f b62ee46
 Author: Mike Linksvayer <mlinksva@github.com>
-Date:   Tue Jul 19 10:24:57 2022 -0700
+Date:   Tue Dec 27 17:22:18 2022 -0800
 
-    Merge pull request #1014 from c0nspiracy/patch-1
+    Merge pull request #1066 from github/bsd-3-nickname
 
-    Fix typo in non-software licenses page
+    Add "Modified" nickname to BSD-3-Clause
 """
 
-CHOOSEALICENSE_COMMIT_HASH = "62dfebe236ac92b5627f4f7cf96650801c5bdb89"
-CHOOSEALICENSE_COMMIT_SHORTHASH = "62dfebe"
+CHOOSEALICENSE_COMMIT_HASH = "0ccd6c5d0ce1c0f68e088ec0c936322e43b2fbfb"
+CHOOSEALICENSE_COMMIT_SHORTHASH = "0ccd6c5"
