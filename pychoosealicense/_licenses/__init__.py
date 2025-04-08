a7519897ecde5dbadb5818a474835dea0ec7f15e
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-04-08 15:11:11 +0100

    Update files from upstream.

--- pychoosealicense/_licenses/__init__.py
+++ pychoosealicense/_licenses/__init__.py
@@ -3,15 +3,15 @@ License metadata used by ``pychoosealicense``.
 """
 
 CHOOSEALICENSE_COMMIT_INFO = """
-commit 005b3dfd323c1e57d0fd0eab7c41b16126f6eb93
-Merge: 080f59b 510b7f2
+commit 26638690a007c584c2d5287a4fbd672eba3631b7
+Merge: 9eb13d3 4caac06
 Author: Mike Linksvayer <mlinksva@github.com>
-Date:   Mon Oct 30 18:23:18 2023 -0700
+Date:   Thu Mar 20 11:08:33 2025 -0700
 
-    Merge pull request #1162 from vorburger/patch-1
+    Merge pull request #1271 from jay-bman725/gh-pages
 
-    Fix Code Search link in CONTRIBUTING
+    Updated the Code of Conduct to be easier to read using Markdown.
 """
 
-CHOOSEALICENSE_COMMIT_HASH = "005b3dfd323c1e57d0fd0eab7c41b16126f6eb93"
-CHOOSEALICENSE_COMMIT_SHORTHASH = "005b3df"
+CHOOSEALICENSE_COMMIT_HASH = "26638690a007c584c2d5287a4fbd672eba3631b7"
+CHOOSEALICENSE_COMMIT_SHORTHASH = "2663869"
