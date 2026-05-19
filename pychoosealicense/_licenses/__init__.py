2815976d75959b6cef8e4d2fc78304dc2284037e
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2026-05-19 20:40:37 +0100

    Update licence data from upstream.

--- pychoosealicense/_licenses/__init__.py
+++ pychoosealicense/_licenses/__init__.py
@@ -3,15 +3,15 @@ License metadata used by ``pychoosealicense``.
 """
 
 CHOOSEALICENSE_COMMIT_INFO = """
-commit 26638690a007c584c2d5287a4fbd672eba3631b7
-Merge: 9eb13d3 4caac06
+commit ed8cbf261d36ead767a8627fc893688bc473fa1c
+Merge: b4442b3 f3b9912
 Author: Mike Linksvayer <mlinksva@github.com>
-Date:   Thu Mar 20 11:08:33 2025 -0700
+Date:   Mon Apr 20 11:48:52 2026 -0700
 
-    Merge pull request #1271 from jay-bman725/gh-pages
+    Merge pull request #1335 from github/misc-fixes
 
-    Updated the Code of Conduct to be easier to read using Markdown.
+    Fix license metadata, link rot, and Rubocop warnings
 """
 
-CHOOSEALICENSE_COMMIT_HASH = "26638690a007c584c2d5287a4fbd672eba3631b7"
-CHOOSEALICENSE_COMMIT_SHORTHASH = "2663869"
+CHOOSEALICENSE_COMMIT_HASH = "ed8cbf261d36ead767a8627fc893688bc473fa1c"
+CHOOSEALICENSE_COMMIT_SHORTHASH = "ed8cbf2"
